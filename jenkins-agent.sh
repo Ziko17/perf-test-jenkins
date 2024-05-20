04b125adea856cac6b18b650a2580ccfd6b1b3ca
amine.bouzayani@elyadata.com <amine.bouzayani@elyadata.com>
2024-05-20 14:10:45 +0100

    Add Jenkinsfile

---
+++ jenkins-agent.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PUB_KEY=$(cat $PUB_KEY_PATH) # Define PUB_KEY_PATH as an environment variable
+AGENT_NAME="perf"
+
+echo $PUB_KEY
+
+docker run -d --rm --name=$AGENT_NAME -p 4022:22 \
+-e "JENKINS_AGENT_SSH_PUBKEY=$PUB_KEY" \
+jenkins/ssh-agent:alpine-jdk17
